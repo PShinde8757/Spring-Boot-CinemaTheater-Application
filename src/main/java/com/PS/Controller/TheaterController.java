a4dac877164aafae287bd5c7b123841e5ff11ba7
Pratik <pratikshinde8757@gmail.com>
2023-03-25 10:26:39 +0530

    update CinemaTheaterApplication for link creation

--- src/main/java/com/PS/Controller/TheaterController.java
+++ src/main/java/com/PS/Controller/TheaterController.java
@@ -4,8 +4,11 @@ package com.PS.Controller;
 import com.PS.CustomException.ShowNotFoundException;
 import com.PS.Model.Theater;
 import com.PS.Repository.TheaterRepository;
+import com.PS.Assembler.TheaterAssembler;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,41 +20,47 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 public class TheaterController {
 
     private final TheaterRepository theaterRepository;
+    private final TheaterAssembler theaterAssembler;
 
-    public TheaterController(TheaterRepository theaterRepository) {
+    public TheaterController(TheaterRepository theaterRepository, TheaterAssembler theaterAssembler) {
         this.theaterRepository = theaterRepository;
+        this.theaterAssembler = theaterAssembler;
     }
 
     @GetMapping("/theater")
     public CollectionModel<EntityModel<Theater>> allShow(){
 
-        List<EntityModel<Theater>> entityModels = (List<EntityModel<Theater>>) theaterRepository.findAll()
+        List<EntityModel<Theater>> entityModels = theaterRepository.findAll()
                 .stream()
-                .map(theater ->EntityModel.of(theater,
+                /*.map(theater ->EntityModel.of(theater,
                         linkTo(methodOn(TheaterController.class).oneShow(theater.getId())).withSelfRel(),
                         linkTo(methodOn(TheaterController.class).allShow()).withRel("All Shows ")))
-                        .collect(Collectors.toList());
-        return CollectionModel.of(entityModels,linkTo(methodOn(TheaterController.class).allShow()).withSelfRel());
+                .collect(Collectors.toList());*/
+                .map(theaterAssembler::toModel)
+                .toList();
+        return CollectionModel.of(entityModels,
+                linkTo(methodOn(TheaterController.class).allShow()).withSelfRel());
     }
 
     @GetMapping("/theater/{id}")
     public EntityModel<Theater> oneShow(@PathVariable Long id){
         Theater theater =theaterRepository.findById(id)
                 .orElseThrow(() -> new ShowNotFoundException(id));
-        return EntityModel.of(theater,
-                linkTo(methodOn(TheaterController.class).oneShow(id)).withSelfRel(),
-                linkTo(methodOn(TheaterController.class).allShow()).withRel("ALl Shows "));
+        return theaterAssembler.toModel(theater);
 
     }
 
     @PostMapping("/theater")
-    Theater newShow(@RequestBody Theater newShow){
-        return theaterRepository.save(newShow);
+    ResponseEntity<?> newShow(@RequestBody Theater newShow){
+        EntityModel<Theater>addShow=theaterAssembler.toModel(theaterRepository.save(newShow));
+        return ResponseEntity
+                .created(addShow.getRequiredLink(IanaLinkRelations.SELF).toUri())
+                .body(addShow);
     }
 
     @PutMapping("/theater/{id}")
-    Theater replaceShow(@RequestBody Theater newShow,@PathVariable Long id){
-        return theaterRepository.findById(id)
+    ResponseEntity<? > replaceShow(@RequestBody Theater newShow,@PathVariable Long id){
+        Theater updateShow= theaterRepository.findById(id)
                 .map(theater -> {
                     theater.setMovieTime(newShow.getMovieTime());
                     theater.setTicketPrice(newShow.getTicketPrice());
@@ -61,10 +70,15 @@ public class TheaterController {
                     newShow.setId(id);
                     return theaterRepository.save(newShow);
                 });
+        EntityModel<Theater> addShow=theaterAssembler.toModel(updateShow);
+        return ResponseEntity
+                .created(addShow.getRequiredLink(IanaLinkRelations.SELF).toUri())
+                .body(addShow);
     }
 
     @DeleteMapping("/theater/{id}")
-    void deleteShow(@PathVariable Long id){
+    ResponseEntity<?> deleteShow(@PathVariable Long id){
          theaterRepository.deleteById(id);
+         return ResponseEntity.noContent().build();
     }
 }
